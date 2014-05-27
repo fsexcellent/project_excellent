c7846dfda695a1550c3c6a20e4892af62ba66cf1
dzc <dzc_woniu@163.com>
2014-05-27 21:42:36 +0800

    modify modifystudent.c

--- modifystudent.c
+++ modifystudent.c
@@ -1,8 +1,50 @@
 #include <stdio.h>
 #include "head.h"
+#include "linklist.h"
+
+void printf_linklist(Linklist *p)
+{
+    while(p->next != NULL)
+    {
+        printf("name\tage\tid\tscore\n");
+        printf("%s\t%d\t%d\t%d\n",p->next->data.name,p->next->data.age,p->next->data.id,p->next->data.score);
+    }
+}
+
+void printf_student(DATATYPE *q)
+{
+    printf("name\tage\tid\tscore\n");
+    printf("%s\t%d\t%d\t%d\n",q->name,q->age,q->id,q->score);
+}
 
 int modify_student(Linklist *L)
 {
-	printf("modify student!\n");
+    //printf("modify student!\n");
+    printf("please into name\n");
+    Linklist *p = NULL;
+    char buf[20];
+    int n = 0;
+    int id = 0;
+    DATATYPE *q = NULL;
+
+    fgets(buf,sizeof(buf),stdin);
+    n = lookup_student_list_by_name(L,buf,&p);
+    if(n = 0)
+    {
+        printf("without the student !\n");
+    }
+    else if(n != 1)
+    {
+        printf("Please into ID who you want to modify !");
+        printf_linklist(p);
+        scanf("%d",&id);
+        q = lookup_student_list_by_id(p,id);
+        printf_student(q);
+    }
+    else if(n = 1)
+    {
+        printf_linklist(p);
+    }
+    
 	return 0;
 }
