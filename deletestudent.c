c28c26aa2a11617ceb977bca970351d42d1e633e
kelly <kelly@ligaili.(none)>
2014-05-28 09:47:30 +0800

    fix issue about deletestudent.c

--- deletestudent.c
+++ deletestudent.c
@@ -6,7 +6,7 @@
 int delete_student(Linklist *L)
 {
     
-    Linklist *head = L->next;
+    Linklist *head;
     Linklist *namelist;
     DATATYPE *stu1;
     char name[100];
@@ -14,22 +14,24 @@ int delete_student(Linklist *L)
     int id;
     int ensure;
     int number_name;
-   
-    if(head == NULL)
-    {
-        puts("Empty Linklist,Please add students first!");
-        return 0;
-    }
-    printf("name\tage\tid\tscore\n");
-
-    while(head->next)
-    {
-        printf("%s\t%d\t%d\t%d\n",head->data.name, head->data.age, head->data.id, head->data.score);
-        head = head->next;
-    }
+  
 
     while(1)
     {
+        head = L->next;
+        if(head == NULL)
+        {
+            puts("Empty Linklist,Please add students first!");
+            return 0;
+        }
+        printf("name\tage\tid\tscore\n");
+
+        while(head)
+        {
+            printf("%s\t%d\t%d\t%d\n",head->data.name, head->data.age, head->data.id, head->data.score);
+            head = head->next;
+        }
+
         puts("Please insert your choice to lookup,1 for id,2 for name,0 for quit!");
         scanf("%d",&choice_n);
         while(getchar() != '\n');
@@ -42,7 +44,7 @@ int delete_student(Linklist *L)
             stu1 = lookup_student_list_by_id(L,id);
             printf("name\tage\tid\tscore\n");
             printf("%s\t%d\t%d\t%d\n",stu1->name, stu1->age, stu1->id, stu1->score);
-
+            delete_student_list(L,stu1);
         }
         else if(2 == choice_n)
         {
@@ -50,7 +52,8 @@ int delete_student(Linklist *L)
             scanf("%s",name);
             while(getchar() != '\n');
 
-            name[strlen(name) - 1] = '\0';
+            name[strlen(name)] = '\0';
+            puts(name);
 
             number_name = lookup_student_list_by_name(L,name,&namelist);
             if(number_name)
@@ -65,10 +68,13 @@ int delete_student(Linklist *L)
             head = namelist->next;
             printf("name\tage\tid\tscore\n");
 
-            while(head->next)
+            while(head)
             {
-               printf("%s\t%d\t%d\t%d\n",head->data.name, head->data.age, head->data.id, head->data.score);
-               head = head->next;
+                *stu1 = head->data;
+                printf("%s\t%d\t%d\t%d\n",stu1->name, stu1->age, stu1->id, stu1->score);
+                delete_student_list(L,stu1);
+
+                head = head->next;
             }
         }
         else if(0 == choice_n)
